5a73ced0899e9c58a283e3f4ec976b2f8029aeca
mjrgeorge <mjrgeorge@gmail.com>
2023-07-16 10:26:34 +0600

    timestams added

--- src/app/modules/book/book.model.ts
+++ src/app/modules/book/book.model.ts
@@ -1,23 +1,31 @@
 import { Schema, model } from 'mongoose';
 import { IBook } from './book.interface';
 
-const bookSchema = new Schema<IBook>({
-  title: {
-    type: String,
-    required: true,
+const bookSchema = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    publicationYear: {
+      type: Number,
+      required: true,
+    },
   },
-  author: {
-    type: String,
-    required: true,
-  },
-  genre: {
-    type: String,
-    required: true,
-  },
-  publicationYear: {
-    type: Number,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 export const Book = model<IBook>('Book', bookSchema);
